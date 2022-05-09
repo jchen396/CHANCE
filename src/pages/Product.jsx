3d26b6e238be0ea893f710be5a05a3451c0c6d3a
Jackie Chen <ecuojackie@yahoo.com>
2022-05-08 19:24:01 -0700

    product page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,142 @@
+import styled from "styled-components"
+import Announcement from "../components/Announcement";
+import Newsletter from "../components/Newsletter";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Add, Remove } from "@material-ui/icons";
+
+const Container  = styled.div``;
+const Wrapper  = styled.div`
+    padding: 50px;
+    display: flex;
+`;
+const ImgContainer  = styled.div`
+    flex:1;
+    display: flex;
+`;
+const Image  = styled.img`
+    width: 50%;
+    height: 90vh;
+    object-fit: cover;
+`;
+const InfoContainer  = styled.div`
+    flex:1;
+    padding: 0px 50px;
+`;
+const Title  = styled.h1`
+    font-weight: 200;
+`;
+const Desc  = styled.p`
+    margin: 20px 0px;
+`;
+const Price  = styled.span`
+    font-weight: 200;
+    font-size: 40px;
+`;
+const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+`;
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`;
+const FilterTitle = styled.div`
+    font-size: 20px;
+    font-weight: 200;
+`;
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+    margin: 0px 5px;
+    cursor: pointer;
+`;
+const FilterSize = styled.select`
+    margin-left: 10px;
+    padding: 5px;
+`;
+const FilterSizeOption = styled.option``;
+const AddContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 50%;
+`;
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+`;
+const Amount = styled.option`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`;
+const Button = styled.button`
+    padding: 15px;
+    border: 2px solid teal;
+    background-color: white;
+    cursor: pointer;
+    font-weight: 500;
+    &:hover{
+        background-color: #f8f4f4;
+    }
+`;
+
+const Product = () => {
+  return (
+    <Container>
+        <Navbar/>
+        <Announcement/>
+        <Wrapper>
+            <ImgContainer>
+                <Image src="https://cdn2.sculpteo.com/blog/wp-content/uploads/2018/05/3Dprintedknit-min.jpg"/>
+                <InfoContainer>
+                    <Title>Sweater</Title>
+                    <Desc>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente dicta obcaecati modi a est aliquam veritatis dolore temporibus cupiditate minima, vel rerum nam soluta adipisci vero commodi nulla sint placeat?</Desc>
+                    <Price>$20</Price>
+                    <FilterContainer>
+                        <Filter>
+                            <FilterTitle>Color</FilterTitle>
+                            <FilterColor color="black"/>
+                            <FilterColor color="darkblue"/>
+                            <FilterColor color="gray"/>
+                        </Filter>
+                        <Filter>
+                            <FilterTitle>Size</FilterTitle>
+                            <FilterSize>
+                            <FilterSizeOption>XS</FilterSizeOption>
+                            <FilterSizeOption>S</FilterSizeOption>
+                            <FilterSizeOption>M</FilterSizeOption>
+                            <FilterSizeOption>L</FilterSizeOption>
+                            <FilterSizeOption>XL</FilterSizeOption>
+                            </FilterSize>
+                        </Filter>
+                    </FilterContainer>
+                    <AddContainer>
+                        <AmountContainer>
+                            <Remove/>
+                            <Amount>1</Amount>
+                            <Add/>
+                        </AmountContainer>
+                        <Button>ADD TO CART</Button>
+                    </AddContainer>
+                </InfoContainer>
+            </ImgContainer>
+        </Wrapper>
+        <Newsletter/>
+        <Footer/>
+    </Container>
+  )
+}
+
+export default Product
