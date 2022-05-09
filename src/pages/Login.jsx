ff141e5548cdcc792db535764e9e26148b817baf
Jackie Chen <ecuojackie@yahoo.com>
2022-05-08 23:05:38 -0700

    registration and login pages

---
+++ src/pages/Login.jsx
@@ -0,0 +1,69 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255,255,255, 0.5),
+        rgba(255,255,255, 0.5)), 
+        url("https://img.freepik.com/free-photo/fashion-model-demonstrate-trendy-wear-accessories-casual-red-jacket-black-short-dress_273443-361.jpg?t=st=1652075764~exp=1652076364~hmac=2201d2fa5c90267ec6bbb393b033b1c56d91440b48d9cfdc04928587df5ff35c&w=996") 
+        center;
+        background-size: cover;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+`;
+const Wrapper = styled.div`
+    width: 40%;
+    padding: 20px;
+    background-color:white;
+`;
+const Title = styled.div`
+    font-size: 24px;
+    font-weight:300;
+`;
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 10px 0px;
+    padding: 10px;
+`;
+const Button = styled.button`
+    width: 40%;
+    border:none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+    margin-bottom: 10px;
+`;
+const Link = styled.a`
+    margin: 5px 0px;
+    font-size: 12px;
+    text-decoration: underline;
+    cursor: pointer;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Title>SIGN INT</Title>
+            <Form>
+            <Input placeholder="username"/>
+            <Input placeholder="password"/>
+            <Button>LOGIN</Button>
+            <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+            <Link>CREATE A NEW ACCOUNT</Link>
+            </Form>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Login
---
+++ src/pages/Register.jsx
@@ -0,0 +1,70 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255,255,255, 0.5),
+        rgba(255,255,255, 0.5)), 
+        url("https://img.freepik.com/free-photo/blonde-woman-with-perfect-wavy-hairstyle-pink-party-dress-posing-hight-heels_273443-1636.jpg?t=st=1652075764~exp=1652076364~hmac=0a3c22f02d415d4dade3189d4bd8cb5dd709f0b4f3d1d92202d3166cd5027017&w=996") 
+        center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+`;
+const Wrapper = styled.div`
+    width: 40%;
+    padding: 20px;
+    background-color:white;
+`;
+const Title = styled.div`
+    font-size: 24px;
+    font-weight:300;
+`;
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+`;
+const Agreement = styled.span`
+    font-size: 12px;
+    margin: 20px 0px;
+`;
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
+`;
+const Button = styled.button`
+    width: 40%;
+    border:none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+`;
+
+const Register = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Title>CREATE AN ACCOUNT</Title>
+            <Form>
+            <Input placeholder="name"/>
+            <Input placeholder="last name"/>
+            <Input placeholder="email"/>
+            <Input placeholder="password"/>
+            <Input placeholder="confirmed password"/>
+            <Agreement>
+                By creating an account, I consent to the processing of my personal
+                data in accordance with the <b>PRIVACY POLICY</b>
+            </Agreement>
+            <Button>CREATE</Button>
+            </Form>
+        </Wrapper>
+    </Container>
+    
+    )
+}
+
+export default Register
