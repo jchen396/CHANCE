f7b7611438e27164d28a6ce75f32e00512bd8d0d
Jackie Chen <ecuojackie@yahoo.com>
2022-05-08 18:28:17 -0700

    shopping category page finished

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -22,6 +22,13 @@ const FilterText = styled.span`
     font-weight: 600;
 `;
 
+const Select = styled.select`
+    padding: 10px;
+    font-weight: 200;
+    margin-right: 20px;
+`;
+const Option = styled.option``;
+
 const ProductList = () => {
   return (
     <Container>
@@ -31,9 +38,38 @@ const ProductList = () => {
         <FilterContainer>
         <Filter>
             <FilterText>Filter Products:</FilterText>
+            <Select>
+                <Option disabled selected>
+                    Color
+                </Option>
+                <Option>White</Option>
+                <Option>Black</Option>
+                <Option>Red</Option>
+                <Option>Blue</Option>
+                <Option>Yellow</Option>
+                <Option>Green</Option>
+            </Select>
+            <Select>
+                <Option disabled selected>
+                    Size
+                </Option>
+                <Option>XS</Option>
+                <Option>S</Option>
+                <Option>M</Option>
+                <Option>L</Option>
+                <Option>XL</Option>
+            </Select>
         </Filter>
         <Filter>
             <FilterText>Sort Products:</FilterText>
+            <Select>
+                <Option selected>
+                    Newest
+                </Option>
+                <Option>Price (asc)</Option>
+                <Option>Price (desc)</Option>
+               
+            </Select>
         </Filter>
         </FilterContainer>
         <Products/>
