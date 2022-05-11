46ab8a36ba23ffbe612189ec856d670849f535c8
Jackie Chen <ecuojackie@yahoo.com>
2022-05-10 18:28:59 -0700

    JWT implementation

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 
@@ -36,9 +37,18 @@ router.post("/login", async (req, res) => {
         originalPassword !== req.body.password &&
             res.status(400).json("Wrong credentials");
 
+        const accessToken = jwt.sign(
+            {
+                id: user._id,
+                isAdmin: user.isAdmin,
+            },
+            process.env.JWT_SEC,
+            { expiresIn: "3d" }
+        );
+
         const { password, ...other } = user._doc;
 
-        res.status(200).json(other);
+        res.status(200).json({ ...other, accessToken });
     } catch (err) {
         res.status(500).json(err);
     }
