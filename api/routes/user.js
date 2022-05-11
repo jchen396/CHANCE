9bb2d5cac9d25845887fe82f86b0bf2bb7c5d9e6
Jackie Chen <ecuojackie@yahoo.com>
2022-05-10 23:12:47 -0700

    JWT token verification

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,27 @@
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-    res.send("user test is successful");
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString();
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.post("/userposttest", (req, res) => {
-    const username = req.body.username;
-    res.send(`Your username is: ${username}.`);
-});
 module.exports = router;
