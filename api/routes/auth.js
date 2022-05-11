16c54dac9ddbd324598c49dfdde7f9f74ca0e7d7
Jackie Chen <ecuojackie@yahoo.com>
2022-05-11 14:45:50 -0700

    User routes

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -46,9 +46,9 @@ router.post("/login", async (req, res) => {
             { expiresIn: "3d" }
         );
 
-        const { password, ...other } = user._doc;
+        const { password, ...others } = user._doc;
 
-        res.status(200).json({ ...other, accessToken });
+        res.status(200).json({ ...others, accessToken });
     } catch (err) {
         res.status(500).json(err);
     }
