e77c6484c7828d51396dce398d4243bd216932ee
Jackie Chen <ecuojackie@yahoo.com>
2022-05-12 16:27:45 -0700

    routing using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,35 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
 
 const App = () => {
-  return <Home/>
+    const user = true;
+
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/products" element={<ProductList />} />
+                <Route path="/products/:id" element={<Product />} />
+                <Route path="/cart" element={<Cart />} />
+
+                <Route
+                    path="/login"
+                    element={user ? <Navigate replace to="/" /> : <Login />}
+                />
+                <Route
+                    path="/register"
+                    element={user ? <Navigate replace to="/" /> : <Register />}
+                />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,24 +1,24 @@
 export const sliderItems = [
     {
-        id:1,
+        id: 1,
         img: "https://www.fashiongonerogue.com/wp-content/uploads/2020/05/Model-Floral-Print-Mini-Dress-Sunglasses-Bag.jpg",
         title: "SUMMER SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg: "#f5fafd"
+        bg: "#f5fafd",
     },
     {
-        id:2,
+        id: 2,
         img: "https://spaceandlight.la/wp-content/uploads/2021/02/ecommerce-on-model-header-3.jpg",
         title: "AUTUMN SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg: "#fcf1ed"
+        bg: "#fcf1ed",
     },
     {
-        id:3,
+        id: 3,
         img: "https://pencilone.com//wp-content/uploads/2018/06/Model-sample-1.jpg",
         title: "LOUNGEWEAR LOVE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg: "#fbf0f4"
+        bg: "#fbf0f4",
     },
 ];
 
@@ -27,50 +27,53 @@ export const categories = [
         id: 1,
         img: "https://images.pexels.com/photos/1485031/pexels-photo-1485031.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         title: "SHIRT STYLE!",
+        cat: "women",
     },
     {
-        id:2,
+        id: 2,
         img: "https://233e5r7tfnv3se11m26d4k8g-wpengine.netdna-ssl.com/wp-content/uploads/2015/08/shutterstock_267639224.jpg",
         title: "LOUNGEWEWAR LOVE",
+        cat: "coat",
     },
     {
-        id:3,
+        id: 3,
         img: "https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         title: "LIGHT JACKETS",
+        cat: "jeans",
     },
 ];
 
 export const popularProducts = [
     {
-        id:1,
-        img:"https://cdn2.sculpteo.com/blog/wp-content/uploads/2018/05/3Dprintedknit-min.jpg",
+        id: 1,
+        img: "https://cdn2.sculpteo.com/blog/wp-content/uploads/2018/05/3Dprintedknit-min.jpg",
     },
     {
-        id:2,
-        img:"https://4.imimg.com/data4/SE/BO/MY-29555283/men-s-3d-print-shirt-500x500.jpg",
+        id: 2,
+        img: "https://4.imimg.com/data4/SE/BO/MY-29555283/men-s-3d-print-shirt-500x500.jpg",
     },
     {
-        id:3,
-        img:"https://5.imimg.com/data5/SY/DA/SV/SELLER-33492231/mufti-surplus-shirt-500x500.jpg",
+        id: 3,
+        img: "https://5.imimg.com/data5/SY/DA/SV/SELLER-33492231/mufti-surplus-shirt-500x500.jpg",
     },
     {
-        id:4,
-        img:"https://m.media-amazon.com/images/I/61LVvVBG3wL._AC_UX385_.jpg",
+        id: 4,
+        img: "https://m.media-amazon.com/images/I/61LVvVBG3wL._AC_UX385_.jpg",
     },
     {
-        id:5,
-        img:"https://n.nordstrommedia.com/id/sr3/c074a4ad-2e1c-4319-a0df-8eb25759315f.jpeg?h=365&w=240&dpr=2",
+        id: 5,
+        img: "https://n.nordstrommedia.com/id/sr3/c074a4ad-2e1c-4319-a0df-8eb25759315f.jpeg?h=365&w=240&dpr=2",
     },
     {
-        id:6,
-        img:"https://zoodmall.com/cdn-cgi/image/w=500,fit=contain,f=auto/https://ae01.alicdn.com/kf/H6a081b5948c0444cae7e418d0d1bb9fe2/Striped-Dress-Lining-Dress-for-Pregnant-Maternity-Women-Clothes-Breastfeeding-Pregnant-Clothes-Pregnancy-Long-Sleeve-Clothes.jpg",
+        id: 6,
+        img: "https://zoodmall.com/cdn-cgi/image/w=500,fit=contain,f=auto/https://ae01.alicdn.com/kf/H6a081b5948c0444cae7e418d0d1bb9fe2/Striped-Dress-Lining-Dress-for-Pregnant-Maternity-Women-Clothes-Breastfeeding-Pregnant-Clothes-Pregnancy-Long-Sleeve-Clothes.jpg",
     },
     {
-        id:7,
-        img:"https://sc04.alicdn.com/kf/H13e0a690283e469aaceaaa38058deff7V.jpg",
+        id: 7,
+        img: "https://sc04.alicdn.com/kf/H13e0a690283e469aaceaaa38058deff7V.jpg",
     },
     {
-        id:8,
-        img:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1635299455-ecowish-women-s-coat-casual-lapel-fleece-1635299451.jpg?crop=1xw:1xh;center,top&resize=480:*",
+        id: 8,
+        img: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1635299455-ecowish-women-s-coat-casual-lapel-fleece-1635299451.jpg?crop=1xw:1xh;center,top&resize=480:*",
     },
 ];
