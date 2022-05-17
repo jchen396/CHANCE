02a5b21d5087aff22c6c2001a2d9a0221988a241
Jackie Chen <ecuojackie@yahoo.com>
2022-05-17 05:23:10 -0700

    redux authentication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,32 @@
-import styled from "styled-components"
-import {mobile} from "../responsive"
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import { login } from "../redux/apiCalls";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
     width: 100vw;
     height: 100vh;
     background: linear-gradient(
-        rgba(255,255,255, 0.5),
-        rgba(255,255,255, 0.5)), 
-        url("https://img.freepik.com/free-photo/fashion-model-demonstrate-trendy-wear-accessories-casual-red-jacket-black-short-dress_273443-361.jpg?t=st=1652075764~exp=1652076364~hmac=2201d2fa5c90267ec6bbb393b033b1c56d91440b48d9cfdc04928587df5ff35c&w=996") 
-        center;
-        background-size: cover;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+            rgba(255, 255, 255, 0.5),
+            rgba(255, 255, 255, 0.5)
+        ),
+        url("https://img.freepik.com/free-photo/fashion-model-demonstrate-trendy-wear-accessories-casual-red-jacket-black-short-dress_273443-361.jpg?t=st=1652075764~exp=1652076364~hmac=2201d2fa5c90267ec6bbb393b033b1c56d91440b48d9cfdc04928587df5ff35c&w=996")
+            center;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 const Wrapper = styled.div`
     width: 40%;
     padding: 20px;
-    background-color:white;
-    ${mobile({width:"75%"})}
+    background-color: white;
+    ${mobile({ width: "75%" })}
 `;
 const Title = styled.div`
     font-size: 24px;
-    font-weight:300;
+    font-weight: 300;
 `;
 const Form = styled.form`
     display: flex;
@@ -37,12 +41,16 @@ const Input = styled.input`
 `;
 const Button = styled.button`
     width: 40%;
-    border:none;
+    border: none;
     padding: 15px 20px;
     background-color: teal;
     color: white;
     cursor: pointer;
     margin-bottom: 10px;
+    &:disabled {
+        color: green;
+        cursor: not-allowed;
+    }
 `;
 const Link = styled.a`
     margin: 5px 0px;
@@ -50,22 +58,45 @@ const Link = styled.a`
     text-decoration: underline;
     cursor: pointer;
 `;
+const Error = styled.span`
+    color: red;
+`;
 
 const Login = () => {
-  return (
-    <Container>
-        <Wrapper>
-            <Title>SIGN INT</Title>
-            <Form>
-            <Input placeholder="username"/>
-            <Input placeholder="password"/>
-            <Button>LOGIN</Button>
-            <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
-            <Link>CREATE A NEW ACCOUNT</Link>
-            </Form>
-        </Wrapper>
-    </Container>
-  )
-}
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+    const { isFetching, error } = useSelector((state) => state.user);
 
-export default Login
+    const handleClick = (e) => {
+        e.preventDefault();
+        login(dispatch, { username, password });
+    };
+
+    return (
+        <Container>
+            <Wrapper>
+                <Title>SIGN IN</Title>
+                <Form>
+                    <Input
+                        placeholder="username"
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <Input
+                        placeholder="password"
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button onClick={handleClick} disabled={isFetching}>
+                        LOGIN
+                    </Button>
+                    {error && <Error>Something went wrong...</Error>}
+                    <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+                    <Link>CREATE A NEW ACCOUNT</Link>
+                </Form>
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default Login;
