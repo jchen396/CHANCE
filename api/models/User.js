875179db494a1dec6e881a4bd226ae90cd6708f7
Jackie Chen <ecuojackie@yahoo.com>
2022-06-08 07:42:56 -0700

    Fetch recent data entries from NodeAPI

--- api/models/User.js
+++ api/models/User.js
@@ -6,6 +6,7 @@ const UserSchema = new mongoose.Schema(
         email: { type: String, require: true, unique: true },
         password: { type: String, require: true },
         isAdmin: { type: Boolean, default: false },
+        img: { type: String },
     },
     {
         timestamps: true,
