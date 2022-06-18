4953b05fb3e768f773e8df8344876db927f51ff5
Jackie Chen <ecuojackie@yahoo.com>
2022-06-18 11:23:10 -0700

    added functiont to add product in admin page using firebase

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAgZ0ueTrVGmHmDQIIKhGwtzeH7w9q55Aw",
+    authDomain: "clover-15b0d.firebaseapp.com",
+    projectId: "clover-15b0d",
+    storageBucket: "clover-15b0d.appspot.com",
+    messagingSenderId: "566014431780",
+    appId: "1:566014431780:web:17d95d5b422714c210feb1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- admin/src/redux/apiCalls.js
+++ admin/src/redux/apiCalls.js
@@ -58,7 +58,7 @@ export const updateProduct = async (id, product, dispatch) => {
 export const addProduct = async (product, dispatch) => {
     dispatch(addProductStart());
     try {
-        const res = await userRequest.post("/products", { product });
+        const res = await userRequest.post("/products", product);
         dispatch(addProductSuccess(res.data));
     } catch (err) {
         dispatch(addProductFailure());
